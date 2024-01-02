fde96d6b432c369e201dc3071bada2a0129c22a5
MackorLab <76862637+MackorLab@users.noreply.github.com>
2024-01-02 10:54:08 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,7 +1,5 @@
-import os
-import subprocess
-import gradio as gr
-from clip_interrogator import Config, Interrogator
+import os, subprocess
+
 def setup():
     install_cmds = [
         ['pip', 'install', 'gradio'],
@@ -10,27 +8,43 @@ def setup():
     ]
     for cmd in install_cmds:
         print(subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8'))
+
 setup()
+
+from IPython.display import display, HTML
+
+title_html = '<h1>Сучонок ты электронный</h1>'
+display(HTML(title_html))
+
 caption_model_name = 'blip-large' #@param ["blip-base", "blip-large", "git-large-coco"]
 clip_model_name = 'ViT-L-14/openai' #@param ["ViT-L-14/openai", "ViT-H-14/laion2b_s32b_b79k"]
+
+import gradio as gr
+from clip_interrogator import Config, Interrogator
+
 config = Config()
 config.clip_model_name = clip_model_name
 config.caption_model_name = caption_model_name
 ci = Interrogator(config)
+
 def image_analysis(image):
     image = image.convert('RGB')
     image_features = ci.image_to_features(image)
+
     top_mediums = ci.mediums.rank(image_features, 5)
     top_artists = ci.artists.rank(image_features, 5)
     top_movements = ci.movements.rank(image_features, 5)
     top_trendings = ci.trendings.rank(image_features, 5)
     top_flavors = ci.flavors.rank(image_features, 5)
+
     medium_ranks = {medium: sim for medium, sim in zip(top_mediums, ci.similarities(image_features, top_mediums))}
     artist_ranks = {artist: sim for artist, sim in zip(top_artists, ci.similarities(image_features, top_artists))}
     movement_ranks = {movement: sim for movement, sim in zip(top_movements, ci.similarities(image_features, top_movements))}
     trending_ranks = {trending: sim for trending, sim in zip(top_trendings, ci.similarities(image_features, top_trendings))}
     flavor_ranks = {flavor: sim for flavor, sim in zip(top_flavors, ci.similarities(image_features, top_flavors))}
+
     return medium_ranks, artist_ranks, movement_ranks, trending_ranks, flavor_ranks
+
 def image_to_prompt(image, mode):
     ci.config.chunk_size = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
     ci.config.flavor_intermediate_count = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
@@ -43,6 +57,7 @@ def image_to_prompt(image, mode):
         return ci.interrogate_fast(image)
     elif mode == 'negative':
         return ci.interrogate_negative(image)
+
 def prompt_tab():
     with gr.Column():
         with gr.Row():
@@ -52,6 +67,7 @@ def prompt_tab():
         prompt = gr.Textbox(label="Prompt")
     button = gr.Button("Generate prompt")
     button.click(image_to_prompt, inputs=[image, mode], outputs=prompt)
+
 def analyze_tab():
     with gr.Column():
         with gr.Row():
@@ -64,7 +80,11 @@ def analyze_tab():
             flavor = gr.Label(label="Flavor", num_top_classes=5)
     button = gr.Button("Analyze")
     button.click(image_analysis, inputs=image, outputs=[medium, artist, movement, trending, flavor])
-with gr.Interface(columns=2, title="Мой интерфейс") as interface:
-    interface.Tab(label="Prompt", fn=prompt_tab)
-    interface.Tab(label="Analyze", fn=analyze_tab)
-    interface.launch(inbrowser=True)
+
+with gr.Blocks() as ui:
+    with gr.Tab("Prompt"):
+        prompt_tab()
+    with gr.Tab("Analyze"):
+        analyze_tab()
+
+ui.launch(debug=True, max_threads=True, share=True, inbrowser=True)
